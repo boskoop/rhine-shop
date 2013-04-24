a36c0bd8ce2077d9b957860d3678a9e7d609426b
Christian Bürgi <mr.boskoop@gmail.com>
2013-04-24 22:52:33 +0200

    modified basic layout

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Bootstrap 101 Template</title>
+  <title>rhine shop</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Bootstrap -->
   <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
@@ -15,10 +15,11 @@
       <div class="span3">
         <h4>Language</h4>
         <p><a href="#"><strong>English</strong></a> |
-          <a href="#">Deutsch</a></p>
+          <a href="#">German</a> |
+          <a href="#">French</a></p>
         </div>
         <div class="span2 text-right">
-          <p><a href="#"><h4>Shopping Cart</h4></a></p>
+          <p><h4>Shopping Cart</h4></p>
           <p><a href="#">2 item(s) - $40.00</a></p>
         </div>
       </div>
@@ -26,18 +27,25 @@
         <div class="span12">
           <div class="navbar navbar-inverse">
             <div class="navbar-inner">
-              <ul class="nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li class="divider-vertical"></li>
-                <li><a href="#">Cart</a></li>
-                <li class="divider-vertical"></li>
-                <li><a href="#">My account</a></li>
-                <li class="divider-vertical"></li>
-                <li><a href="#">Information</a></li>
-              </ul>
-              <form class="navbar-search pull-right" action="">
-                <input type="text" class="search-query span3" placeholder="Search" />
-              </form>
+              <div class="container">
+                <div class="nav-collapse">
+                  <ul class="nav">
+                    <li class="active"><a href="#">Shop</a></li>
+                    <li class="divider-vertical"></li>
+                    <li><a href="#">Cart</a></li>
+                    <li class="divider-vertical"></li>
+                    <li><a href="#">My account</a></li>
+                    <li class="divider-vertical"></li>
+                    <li><a href="#">Information</a></li>
+                  </ul>
+                  <a href="#" class="btn btn-inverse pull-right">Logout</a>
+                  <span class="divider-vertical pull-right">&nbsp;</span>
+                  <form class="navbar-search pull-right" action="">
+                      <input type="text" class="search-query span3" placeholder="Search" />
+                  </form>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -54,11 +62,91 @@
               <li><a href="#">New arrivals</a></li>
               <li><a href="#">Bestsellers</a></li>
             </ul>
+            <li class="divider"></li>
             <li><a href="#">Playstation 3</a></li>
+            <li class="divider"></li>
             <li><a href="#">Xbox 360</a></li>
           </ul>
         </div>
         <div class="span9">
+          <ul class="breadcrumb well">
+            <li><a href="#">Shop</a><span class="divider">/</span></li>
+            <li class="active">PC Games</li>
+          </ul>
+          <ul class="thumbnails">
+            <li class="span3">
+              <div class="thumbnail">
+                <a class="text-center" href="#">
+                  <img src="img/barcode_150.jpg" alt="" />
+                </a>
+                <div class="caption">
+                  <a href="#">
+                    <h3>Product 1</h3>
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li class="span3">
+              <div class="thumbnail">
+                <a class="text-center" href="#">
+                  <img src="img/barcode_150.jpg" alt="" />
+                </a>
+                <div class="caption">
+                  <a href="#">
+                    <h3>Product 2</h3>
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li class="span3">
+              <div class="thumbnail">
+                <a class="text-center" href="#">
+                  <img src="img/barcode_150.jpg" alt="" />
+                </a>
+                <div class="caption">
+                  <a href="#">
+                    <h3>Product 3</h3>
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li class="span3">
+              <div class="thumbnail">
+                <a class="text-center" href="#">
+                  <img src="img/barcode_150.jpg" alt="" />
+                </a>
+                <div class="caption">
+                  <a href="#">
+                    <h3>Product 4</h3>
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li class="span3">
+              <div class="thumbnail">
+                <a class="text-center" href="#">
+                  <img src="img/barcode_150.jpg" alt="" />
+                </a>
+                <div class="caption">
+                  <a href="#">
+                    <h3>Product 5</h3>
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li class="span3">
+              <div class="thumbnail">
+                <a class="text-center" href="#">
+                  <img src="img/barcode_150.jpg" alt="" />
+                </a>
+                <div class="caption">
+                  <a href="#">
+                    <h3>Product 6</h3>
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
