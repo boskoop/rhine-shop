b3a5f682799bf69658d6c6a281dcd16cc86760ba
Christian Bürgi <mr.boskoop@gmail.com>
2013-05-22 22:06:57 +0200

    added tests for routes

---
+++ application/libraries/tests/routetestcase.php
@@ -0,0 +1,82 @@
+<?php namespace Tests;
+
+use PHPUnit_Framework_TestCase;
+use Laravel\CLI\Command;
+use Laravel\Session;
+use Laravel\Cookie;
+use Laravel\Config;
+
+abstract class RouteTestCase extends PHPUnit_Framework_TestCase {
+
+	private $tempSessionDriver;
+
+	protected final function setUp()
+	{
+		echo "\nRouteTestCase: running ".get_class($this)."->".$this->getName()."()";
+
+		PersistenceTestHelper::cleanDatabase();
+
+		// Initialize the session (session is retrieved via cookie)
+		$tempSessionDriver = Config::get('session.driver');
+		Config::set('session.driver', 'database');
+		Session::load();
+
+		// Seed test-data
+		Command::run(array('seed_data'));
+		echo "\n";
+
+		$this->setUpInternal();
+	}
+
+	protected final function tearDown()
+	{
+		// Reset session driver
+		Config::set('session.driver', $this->tempSessionDriver);
+
+		$this->tearDownInternal();
+	}
+
+	/**
+	 * Template method, is called on PHPUnit setUp().
+	 *
+	 * @return void
+	 */
+	protected function setUpInternal() { }
+
+	/**
+	 * Template method, is called on PHPUnit tearDown().
+	 *
+	 * @return void
+	 */
+	protected function tearDownInternal() { }
+
+	/*
+	 * Simulates a GET request to the router.
+	 */
+	protected function httpGet($route)
+	{
+		return $this->callHttp('GET', $route);
+	}
+
+	/*
+	 * Simulates a POST request to the router.
+	 */
+	protected function httpPost($route)
+	{
+		return $this->callHttp('POST', $route);
+	}
+
+	private function callHttp($method, $route)
+	{
+		echo "\nRouteTestCase: calling HTTP ".$method." '".$route."'";
+
+		$request = \Router::route($method, $route);
+		\Request::setMethod($method);
+
+		$response = $request->call();
+		$foundation = $response->foundation;
+		echo "\nRouteTestCase: response HTTP/".$foundation->getProtocolVersion()." ".$foundation->getStatusCode()."\n";
+		return $response;
+	}
+
+}
---
+++ application/tests/routes/shoproutes.test.php
@@ -0,0 +1,55 @@
+<?php
+
+class ShopRoutesTest extends Tests\RouteTestCase
+{
+
+	/**
+	 * Tests the home-route.
+	 *
+	 * @return void
+	 */
+	public function testHome()
+	{
+		$response = $this->httpGet('/');
+		$this->assertTrue($response->foundation->isOk());
+	}
+
+	/**
+	 * Tests the shop-route.
+	 *
+	 * @return void
+	 */
+	public function testShop()
+	{
+		$response = $this->httpGet('shop');
+		$this->assertTrue($response->foundation->isOk());
+	}
+
+	/**
+	 * Tests the category-route. Expects 'seed_data' to create at least a category.
+	 *
+	 * @return void
+	 */
+	public function testCategory()
+	{
+		$category = Category::first();
+
+		$response = $this->httpGet('category/'.$category->id);
+		$this->assertTrue($response->foundation->isOk());
+	}
+
+	/**
+	 * Tests the category-route. Expects 'seed_data' not to create a category with
+	 * id 100.
+	 *
+	 * @return void
+	 */
+	public function testCategoryNotFound()
+	{
+		$this->assertNull(Category::find(100));
+
+		$response = $this->httpGet('category/100');
+		$this->assertTrue($response->foundation->isNotFound());
+	}
+
+}
