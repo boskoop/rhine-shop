1f39937a6959094d1b250d1b6c43b2138d050eeb
Christian Bürgi <mr.boskoop@gmail.com>
2013-05-25 17:02:16 +0200

    added dto tests

--- application/tests/domainmodels/cart/cartdto.test.php
+++ application/tests/domainmodels/cart/cartdto.test.php
@@ -1,7 +1,6 @@
 <?php
 
 use Rhine\DomainModels\Cart\CartDto;
-use Rhine\DomainModels\Cart\CartPositionDto;
 use Rhine\DomainModels\Cart\Impl\CartImpl;
 
 class CartDtoTest extends Tests\UnitTestCase
@@ -21,9 +20,45 @@ class CartDtoTest extends Tests\UnitTestCase
 		$cart = new CartImpl($productRepositoryMock);
 
 		$dto = CartDto::createFromCart($cart);
-		
+
 		$positionsDtoArray = $dto->getPositions();
 		$this->assertEquals(0, sizeof($positionsDtoArray));
+	}
+
+	public function testCreateFromCart()
+	{
+		$productRepositoryMock = $this->getMock('Rhine\Repositories\ProductRepository');
+		$cart = new CartImpl($productRepositoryMock);
+		$cart->addPosition(1);
+		$cart->addPosition(2);
+
+		$dto = CartDto::createFromCart($cart);
+		
+		$positionsDtoArray = $dto->getPositions();
+		$this->assertEquals(2, sizeof($positionsDtoArray));
+	}
+
+	public function testCartPositionDtoQuantities()
+	{
+		$productRepositoryMock = $this->getMock('Rhine\Repositories\ProductRepository');
+		$cart = new CartImpl($productRepositoryMock);
+		$cart->addPosition(2);
+		$cart->addPosition(2);
+		$cart->addPosition(1);
+		$cart->addPosition(3);
+		$cart->addPosition(3);
+		$cart->addPosition(3);
+
+		$dto = CartDto::createFromCart($cart);
+		
+		$positionsDtoArray = $dto->getPositions();
+		$this->assertEquals(3, sizeof($positionsDtoArray));
+		$this->assertEquals(2, $positionsDtoArray[0]->getId());
+		$this->assertEquals(2, $positionsDtoArray[0]->getQuantity());
+		$this->assertEquals(1, $positionsDtoArray[1]->getId());
+		$this->assertEquals(1, $positionsDtoArray[1]->getQuantity());
+		$this->assertEquals(3, $positionsDtoArray[2]->getId());
+		$this->assertEquals(3, $positionsDtoArray[2]->getQuantity());
 	}
 
 }
