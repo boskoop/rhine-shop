5af1d6e04eb34826d4c543fe911be387ea1bfeed
Christian Bürgi <mr.boskoop@gmail.com>
2013-06-08 17:32:22 +0200

    adding tests for subtracting/deleting cart positions

--- application/tests/routes/cartroutes.test.php
+++ application/tests/routes/cartroutes.test.php
@@ -35,4 +35,61 @@ class CartRoutesTest extends Tests\RouteTestCase
 		$this->assertTrue($response->foundation->isOk());
 	}
 
+	/**
+	 * Tests subtracting a product from the cart.
+	 *
+	 * @return void
+	 */
+	public function testSubtractFromCart()
+	{
+		$cart = Ioc::resolve('cartService')->loadCart();
+		$cart->addPosition(1);
+		$cart->addPosition(1);
+		$this->assertFalse($cart->isEmpty());
+		$this->assertEquals(2, $cart->getTotalQuantity());
+
+		$response = $this->httpPost('cart/sub/1', array('csrf_token' => Session::token()));
+		$this->assertTrue($response->foundation->isRedirect());
+		$cart = Ioc::resolve('cartService')->loadCart();
+		$this->assertFalse($cart->isEmpty());
+		$this->assertEquals(1, $cart->getTotalQuantity());
+
+		$response = $this->httpPost('cart/sub/1', array('csrf_token' => Session::token()));
+		$this->assertTrue($response->foundation->isRedirect());
+		$cart = Ioc::resolve('cartService')->loadCart();
+		$this->assertTrue($cart->isEmpty());
+
+		$response = $this->httpGet('cart');
+		$this->assertTrue($response->foundation->isOk());
+	}
+
+	/**
+	 * Tests deleting a product from the cart.
+	 *
+	 * @return void
+	 */
+	public function testClearFromCart()
+	{
+		$cart = Ioc::resolve('cartService')->loadCart();
+		$cart->addPosition(1);
+		$cart->addPosition(1);
+		$cart->addPosition(2);
+		$this->assertFalse($cart->isEmpty());
+		$this->assertEquals(3, $cart->getTotalQuantity());
+
+		$response = $this->httpPost('cart/del/1', array('csrf_token' => Session::token()));
+		$this->assertTrue($response->foundation->isRedirect());
+		$cart = Ioc::resolve('cartService')->loadCart();
+		$this->assertFalse($cart->isEmpty());
+		$this->assertEquals(1, $cart->getTotalQuantity());
+
+		$response = $this->httpPost('cart/del/2', array('csrf_token' => Session::token()));
+		$this->assertTrue($response->foundation->isRedirect());
+		$cart = Ioc::resolve('cartService')->loadCart();
+		$this->assertTrue($cart->isEmpty());
+
+		$response = $this->httpGet('cart');
+		$this->assertTrue($response->foundation->isOk());
+	}
+
 }
